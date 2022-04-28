d7dfc28987791e98a1f68fc83e571ada25d7f20b
joshw10 <43585348+joshw10@users.noreply.github.com>
2022-04-28 07:43:13 -0400

    Adding comments to SearchThread.cpp

--- src/widgets/SearchThread.cpp
+++ src/widgets/SearchThread.cpp
@@ -1,12 +1,14 @@
 #include "SearchThread.h"
 #include <QDebug>
 
+//Constructor to register QList<SearchDescription> as a Meta Type to retrieve data between threads
 SearchThread::SearchThread(QObject *parent) :
     QThread(parent)
 {
     qRegisterMetaType<QList<SearchDescription>>("QList<SearchDescription>");
 }
 
+//Set variables that will be used to search 
 void SearchThread::setVariables(QList<SearchDescription> search, QString si, QString sf, QString ss)
 {
 
@@ -16,6 +18,7 @@ void SearchThread::setVariables(QList<SearchDescription> search, QString si, QSt
     searchS = ss;
 }
 
+//Run thread to search based on users input
 void SearchThread::run()
 {
     sch = Core()->getAllSearch(searchF, searchS, searchI);
